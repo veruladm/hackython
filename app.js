337e8a61ed02d59bbf9bb33e7457961c218d07e6
Daryl <veruladm@ph.ibm.com>
2015-08-27 20:56:04 +0800

    templates and routes added

--- app.js
+++ app.js
@@ -7,6 +7,15 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+//require HTTP
+var http = require('http');
+
+// // create server
+// http.createServer(function(req,res){
+// 	res.end('Hello World!');
+// }).listen(8000,'127.0.0.1');
+
+// console.log('Server running at port localhost:8000');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -15,15 +24,36 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+// default engine
+app.engine('html',require('ejs').renderFile);
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+//app.set('views', __dirname + '/public');
+
+// app.use(express.static(path.join(__dirname + 'public')));
+
+app.get("/",function(req,res){
+	res.render('index.html',null);
+});
+app.get("/typhoon",function(req,res){
+	res.render('typhoon.html',null);
+});
+app.get("/earthquake",function(req,res){
+	res.render('earthquake.html',null);
+});
+app.get("/contacts",function(req,res){
+	res.render('contacts.html',null);
+});
 
 // get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+// var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, function() {
+var server = app.listen(8000, function() {
 
+  var host = server.address().address;
+  var port = server.address().port;
 	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+  console.log("Example app listening at http://" + host + " :", host, port);
 });
